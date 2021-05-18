d45755aadde2e270a8d53acc239d7e31e625aee4
Jakub <jakub.szaredko@gmail.com>
2021-05-19 00:17:30 +0200

    Change the mobile view of navigation, add a close button

--- dist/js/navigation.js
+++ dist/js/navigation.js
@@ -1,17 +1,23 @@
 const nav = document.querySelector('.navigation');
 const menuButton = document.querySelector('.navigation__menu-button');
+const menuCloseButton = document.querySelector('.navigation__menu-close-button');
 const menuBg = document.querySelector('.navigation__menu-bg');
 
 if (menuButton) {
     menuButton.addEventListener('click', () => {
         nav.classList.toggle('open');
-        menuButton.classList.toggle('clicked');
+    });
+}
+
+if (menuCloseButton) {
+    menuCloseButton.addEventListener('click', () => {
+        nav.classList.toggle('open');
     });
 }
 
 if (menuBg) {
     menuBg.addEventListener('click', () => {
         nav.classList.toggle('open');
-        menuButton.classList.toggle('clicked');
+        //menuButton.classList.toggle('clicked');
     });
 }
