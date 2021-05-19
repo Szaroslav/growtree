63bae8065ea09c950b9107d18f5ba2c97fe7907a
Jakub <jakub.szaredko@gmail.com>
2021-05-19 22:23:46 +0200

    Add a swipe feature

--- dist/js/navigation.js
+++ dist/js/navigation.js
@@ -1,8 +1,13 @@
+/* ======= Elements and variables ======= */
 const nav = document.querySelector('.navigation');
 const menuButton = document.querySelector('.navigation__menu-button');
 const menuCloseButton = document.querySelector('.navigation__menu-close-button');
 const menuBg = document.querySelector('.navigation__menu-bg');
 
+let down = new Object();
+let up = new Object();
+
+/* ======= Elements events ======= */
 if (menuButton) {
     menuButton.addEventListener('click', () => {
         nav.classList.toggle('open');
@@ -18,6 +23,22 @@ if (menuCloseButton) {
 if (menuBg) {
     menuBg.addEventListener('click', () => {
         nav.classList.toggle('open');
-        //menuButton.classList.toggle('clicked');
     });
 }
+
+/* ======= Swipe event ======= */
+document.addEventListener('touchstart', (e) => {
+    down.x = e.changedTouches[0].clientX;
+    down.y = window.innerHeight - e.changedTouches[0].clientY;
+});
+
+document.addEventListener('touchend', (e) => {
+    up.x = e.changedTouches[0].clientX;
+    up.y = window.innerHeight - e.changedTouches[0].clientY; 
+
+    const diff = { x: up.x - down.x, y: up.y - down.y }
+    const angle = Math.atan2(diff.y, diff.x) / Math.PI;
+    
+    if (down.x <= window.innerWidth / 10 && angle <= 0.1 && angle >= -0.1)
+        nav.classList.add('open');
+});
